dcb6f1bc516ed5f4b48da0d4cd2c3e208cf02f96
tebr1z <tabrizyh@code.edu.az>
2024-03-28 17:20:28 +0400

    Proje dosyası ekle.

---
+++ HomeTaskDelgate/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskDelgate
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public double Salary { get; set; }
+    }
+
+}
---
+++ HomeTaskDelgate/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HomeTaskDelgate
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Person> persons = new List<Person>
+        {
+            new Person { Name = "Rauf", Surname = "Memmedov", Address = "Bakı", Salary = 1200 },
+            new Person { Name = "Nergiz", Surname = "Eliyeva", Address = "Gence", Salary = 9000 },
+            new Person { Name = "Vusal", Surname = "Quliyev", Address = "Sumqayıt", Salary = 1500 },
+            new Person { Name = "Ayşe", Surname = "Hüseynova", Address = "Lenkeran", Salary = 800 }
+        };
+
+         
+            List<Person> filteredPersons = FilterPersons(persons, p => p.Salary > 1000);
+
+         
+            foreach (var person in filteredPersons)
+            {
+                Console.WriteLine($"Ad: {person.Name}, Soyad: {person.Surname}, Ünvan: {person.Address}");
+            }
+        }
+
+        static List<Person> FilterPersons(List<Person> persons, Predicate<Person> predicate)
+        {
+            List<Person> filteredPersons = new List<Person>();
+
+            foreach (var person in persons)
+            {
+                if (predicate(person))
+                {
+                    filteredPersons.Add(person);
+                }
+            }
+
+            return filteredPersons;
+        }
+    }
+
+}
