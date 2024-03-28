da07bddd4d8d7bd02d2ee21b80a48a9e4b238359
tebr1z <tabrizyh@code.edu.az>
2024-03-28 17:36:53 +0400

    Feat task 1,2,3

---
+++ HomeTaskDelgate/EmployeeHelper.cs
@@ -0,0 +1,14 @@
+﻿using HomeTaskDelgate;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EmployeeHelper
+{
+    public static double CalculateAverageSalary(List<Employee> employees, int minAge, int maxAge)
+    {
+        var selectedEmployees = employees.Where(e => e.Age >= minAge && e.Age <= maxAge);
+        double averageSalary = selectedEmployees.Select(e => e.Salary).DefaultIfEmpty(0).Average();
+        return averageSalary;
+    }
+}
+
--- HomeTaskDelgate/Program.cs
+++ HomeTaskDelgate/Program.cs
@@ -1,43 +1,70 @@
 ﻿namespace HomeTaskDelgate
 {
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-            
-            List<Person> persons = new List<Person>
+            #region Task1
+            //    List<Person> persons = new List<Person>
+            //{
+            //    new Person { Name = "Rauf", Surname = "Memmedov", Address = "Bakı", Salary = 1200 },
+            //    new Person { Name = "Nergiz", Surname = "Eliyeva", Address = "Gence", Salary = 9000 },
+            //    new Person { Name = "Vusal", Surname = "Quliyev", Address = "Sumqayıt", Salary = 1500 },
+            //    new Person { Name = "Ayşe", Surname = "Hüseynova", Address = "Lenkeran", Salary = 800 }
+            //};
+
+
+            //    List<Person> filteredPersons = FilterPersons(persons, p => p.Salary > 1000);
+
+
+            //    foreach (var person in filteredPersons)
+            //    {
+            //        Console.WriteLine($"Ad: {person.Name}, Soyad: {person.Surname}, Ünvan: {person.Address}");
+            //    }
+
+            //    static List<Person> FilterPersons(List<Person> persons, Predicate<Person> predicate)
+            //    {
+            //        List<Person> filteredPersons = new List<Person>();
+
+            //        foreach (var person in persons)
+            //        {
+            //            if (predicate(person))
+            //            {
+            //                filteredPersons.Add(person);
+            //            }
+            //        }
+
+            //        return filteredPersons;
+            //    }
+
+            #endregion
+
+            #region Task2
+
+
+            #endregion
+
+            #region Task3
+
+            List<Employee> employees = new List<Employee>
         {
-            new Person { Name = "Rauf", Surname = "Memmedov", Address = "Bakı", Salary = 1200 },
-            new Person { Name = "Nergiz", Surname = "Eliyeva", Address = "Gence", Salary = 9000 },
-            new Person { Name = "Vusal", Surname = "Quliyev", Address = "Sumqayıt", Salary = 1500 },
-            new Person { Name = "Ayşe", Surname = "Hüseynova", Address = "Lenkeran", Salary = 800 }
+            new Employee { Name = "Ali", Age = 25, Salary = 2500 },
+            new Employee { Name = "Nigar", Age = 30, Salary = 3000 },
+            new Employee { Name = "Vasif", Age = 22, Salary = 2000 },
+            new Employee { Name = "Ayten", Age = 35, Salary = 3500 },
+            new Employee { Name = "Elvin", Age = 40, Salary = 4000 }
         };
 
-         
-            List<Person> filteredPersons = FilterPersons(persons, p => p.Salary > 1000);
+            double averageSalary = EmployeeHelper.CalculateAverageSalary(employees, 20, 40);
+            Console.WriteLine($"20-40 yaş arasındaki işçilerin maaşlarının ortalaması: {averageSalary}");
 
-         
-            foreach (var person in filteredPersons)
-            {
-                Console.WriteLine($"Ad: {person.Name}, Soyad: {person.Surname}, Ünvan: {person.Address}");
-            }
+            #endregion
+
+
         }
 
-        static List<Person> FilterPersons(List<Person> persons, Predicate<Person> predicate)
-        {
-            List<Person> filteredPersons = new List<Person>();
 
-            foreach (var person in persons)
-            {
-                if (predicate(person))
-                {
-                    filteredPersons.Add(person);
-                }
-            }
-
-            return filteredPersons;
-        }
     }
 
 }
